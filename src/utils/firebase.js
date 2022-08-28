dc28f2fa0530f907741baa0881b548c5ba8a890c
Miav <2021171015@uteq.edu.mx>
2022-08-28 15:15:10 -0500

    env actualizado

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,16 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCBV7K9Nd7_KgIBMZvyEggmmhbWWZC7dAE",
-  authDomain: "gplay-721ce.firebaseapp.com",
-  projectId: "gplay-721ce",
-  storageBucket: "gplay-721ce.appspot.com",
-  messagingSenderId: "239352395437",
-  appId: "1:239352395437:web:8c651df1de33dbd85e7824"
+  apiKey: "AIzaSyD7dXByO3L4YNYfmKcY3_LZ10pSVMfIyDs",
+  authDomain: "gplay-e0503.firebaseapp.com",
+  projectId: "gplay-e0503",
+  storageBucket: "gplay-e0503.appspot.com",
+  messagingSenderId: "769883964731",
+  appId: "1:769883964731:web:eb4dab5240a0c89294631e"
 };
 
 // Initialize Firebase
--- src/vistas/MapaVista.js
+++ src/vistas/MapaVista.js
@@ -3,6 +3,7 @@ import * as Location from 'expo-location';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView, { Marker, Polyline } from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
+import {GOOGLE_MAPS_KEY} from '@env';
 const carImage = require('../../assets/img/car.png')
 
 export function MapaVista() {
@@ -11,6 +12,43 @@ export function MapaVista() {
     latitude: 20.6539495,
     longitude: -100.4082825,
   });
+  const [place1, setPlace1] = React.useState({
+    latitude: 20.6539495,
+    longitude: -100.4082825,
+  });
+  const [place2, setPlace2] = React.useState({
+    latitude: 20.5763931,
+    longitude: -100.420089,
+  });
+
+   const [place3, setPlace3] = React.useState({
+    latitude: 20.6045147,
+    longitude: -100.4520181,
+  });
+
+
+  const [destination, setDestination] = React.useState({
+    latitude: 33.753746,
+    longitude: -84.386330,
+  });
+
+  React.useEffect(() => {
+    getLocationPermission();
+  }, [])
+
+  async function getLocationPermission() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if(status !== 'granted') {
+      alert('Permission denied');
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    const current = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude
+    }
+    setOrigin(current);
+  }
 
   return (
     <View style={styles.container}>
@@ -20,10 +58,39 @@ export function MapaVista() {
         longitude: origin.longitude,
         latitudeDelta: 0.09,
         longitudeDelta: 0.04
-      }}
+      }}>
+
+      <Marker 
+      draggable
+      coordinate={origin}
+      image={carImage}
+      onDragEnd={(direction) => setOrigin(direction.nativeEvent.coordinate)}
     />
+    <Marker 
+      draggable
+      coordinate={place1}
+      onDragEnd={(direction) => setPlace3(direction.nativeEvent.coordinate)}
+    />
+     <Marker 
+      draggable
+      coordinate={place2}
+      onDragEnd={(direction) => setPlace3(direction.nativeEvent.coordinate)}
+    />
+     <Marker 
+      draggable
+      coordinate={place3}
+      onDragEnd={(direction) => setPlace3(direction.nativeEvent.coordinate)}
+    />
+    <MapViewDirections
+      origin={origin}
+      destination={place3}
+      apikey={GOOGLE_MAPS_KEY}
+      strokeColor="blue"
+      strokeWidth={4}
+    />
+    
         
-        
+    </MapView>   
     </View>
   );
 }
